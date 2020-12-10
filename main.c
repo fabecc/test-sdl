6583d5e7266699f5d391fb08cefc202395d84f4a
Fabrice Hesling <fabecc@iFabecc.local>
2020-12-10 17:16:19 +0100

    Add code

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 int main(void)
 {
     printf("Start\n");
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    {
+        printf("Failed to init SDL: %s\n",SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* pWindow = SDL_CreateWindow("My first app",
+                                           SDL_WINDOWPOS_UNDEFINED,
+                                           SDL_WINDOWPOS_UNDEFINED,
+                                           640,
+                                           480,
+                                           SDL_WINDOW_SHOWN);
+    if (pWindow == NULL) {
+        printf("Failed to create windows: %s\n", SDL_GetError());
+        return 2;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(pWindow);
+    SDL_Quit();
+
     return 0;
 }
