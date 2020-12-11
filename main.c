ce310cb09e58781510ce65edb519ad68181437e5
Fabrice Hesling <fabecc@gmail.com>
2020-12-11 16:44:23 +0100

    Improve code to have interaction with user

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <SDL.h>
+#include <time.h>
 
 int main(void)
 {
     printf("Start\n");
+    srand((unsigned int)time(NULL));
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0 )
     {
@@ -22,7 +25,36 @@ int main(void)
         return 2;
     }
 
-    SDL_Delay(3000);
+    /* Creating the surface. */
+    SDL_Surface* s = SDL_GetWindowSurface(pWindow);
+
+    /* Filling the surface with red color. */
+    SDL_FillRect(s, NULL, SDL_MapRGB(s->format, 0, 0, 255));
+    SDL_UpdateWindowSurface(pWindow);
+
+    SDL_Event event;	 // used to store any events from the OS
+    int running = 1; // used to determine if we're running the game
+
+    while (running)
+    {
+        // poll for events from SDL
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                running = 0;
+                break;
+
+            case SDL_MOUSEBUTTONUP:
+                SDL_FillRect(s, NULL, SDL_MapRGB(s->format, rand() % 255, rand() % 255, rand() % 255));
+                SDL_UpdateWindowSurface(pWindow);
+                break;
+            }
+        }
+
+    }
+
 
     SDL_DestroyWindow(pWindow);
     SDL_Quit();
